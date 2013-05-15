a5e8979784ad8c3ca351785676de1d059cf86847
Elad Moshe <elad.moshe@hp.com>
2013-05-15 23:48:46 +0300

    improved controller to handle email as well

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,12 +5,22 @@
 angular.module('myApp.controllers', []).
     controller('welcomeCtrl', ['$scope','$location', function($scope,$location) {
         $scope.password = {text: null};
-        $scope.$watch("password.text", function(newVal) {
+        $scope.email = {text: null};
+
+        function checkAndSetPath (newVal) {
             if (newVal && newVal.length > 2) {
                 $location.path('/warning');
             }
+        }
+
+        $scope.$watch("password.text", function(newVal) {
+            checkAndSetPath(newVal);
         });
-        $scope.validate = function(){
+        $scope.$watch("email.text", function(newVal) {
+            checkAndSetPath(newVal);
+        });
+
+        $scope.submit = function(){
 
         }
 
