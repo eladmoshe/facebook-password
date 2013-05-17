c64cac15f50ae2266124ba37cadf68554daf0841
Elad Moshe <elad.moshe@hp.com>
2013-05-17 14:59:51 +0300

    added flash when user type on screen

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,19 +1,33 @@
 angular.module('myApp.directives', [])
     .directive("fbLogin", function($rootScope) {
         return function(scope, iElement, iAttrs) {
+            console.log(FB);
             if (FB) {
-                FB.XFBML.parse(iElement[0]);
+                FB.XFBML.parse();
             }
         };
     })
-    .directive('setHtmlClass', function(){
-        return function(scope, element, attrs){
+    .directive('setHtmlClass', function() {
+        return function(scope, element, attrs) {
             if (attrs.isClear === 'true') {
                 $('html').removeClass(attrs.className);
             } else {
                 $('html').addClass(attrs.className);
             }
-
+        }
+    })
+    .directive('keyboardFlash', function() {
+        return function(scope, element, attrs) {
+            var body = $('body');
+            body.keydown(function() {
+                $('html').addClass(attrs.className);
+            });
+            body.keyup(function() {
+                $('html').removeClass(attrs.className);
+            });
+            element.bind('$destroy', function() {
+                body.unbind('keydown');
+            });
         }
     })
 ;
