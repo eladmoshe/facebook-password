151fcbe2967ce1c312ca142bfa441bcb543e485e
Elad Moshe <elad.moshe@hp.com>
2013-05-17 13:44:39 +0300

    created directive to update page background added jquery fixed fb directive

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.controllers']).
+angular.module('myApp', ['myApp.filters', 'myApp.controllers','myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'app/partials/welcome-page.html', controller: 'welcomeCtrl'});
     $routeProvider.when('/warning', {templateUrl: 'app/partials/warning-page.html', controller: 'warningCtrl'});
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,7 +1,19 @@
-angular.module('myApp', []).directive("fbLogin", function($rootScope) {
-    return function(scope, iElement, iAttrs) {
-        if (FB) {
-            FB.XFBML.parse(iElement[0]);
+angular.module('myApp.directives', [])
+    .directive("fbLogin", function($rootScope) {
+        return function(scope, iElement, iAttrs) {
+            if (FB) {
+                FB.XFBML.parse(iElement[0]);
+            }
+        };
+    })
+    .directive('setHtmlClass', function(){
+        return function(scope, element, attrs){
+            if (attrs.isClear === 'true') {
+                $('html').removeClass(attrs.className);
+            } else {
+                $('html').addClass(attrs.className);
+            }
+
         }
-    };
-});
+    })
+;
